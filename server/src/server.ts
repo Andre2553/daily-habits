4b7651f7082375ba0ec766338ec2a2cded973013
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2023-01-19 15:45:28 +1100

    server@update - New models & routes
    - new models on prisma
    - 2 new routes  /habits & /day

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,13 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { prisma } from './lib/prisma'
+import { appRoutes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/', async () => {
-  const habits = await prisma.habit.findMany()
-
-  return habits
-})
 
 app.listen({
   port: 3333,
